01794260a790ec76cf1ab855a64a74765fe12e52
Gabriel Ron <gaboarp2020@gmail.com>
2021-10-17 14:36:27 +0200

    Ignore All Logs

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import {AppRegistry} from 'react-native';
+import {LogBox} from 'react-native';
 
 import App from './src/App';
 import {name as appName} from './app.json';
 
+LogBox.ignoreAllLogs();
 AppRegistry.registerComponent(appName, () => App);
